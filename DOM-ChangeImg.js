025bedee92d715aa09c0297ccdfa90ff6a295125
jdluis <92221396+jdluis@users.noreply.github.com>
2022-01-09 15:02:56 +0000

    Update DOM-ChangeImg.js

--- DOM-ChangeImg.js
+++ DOM-ChangeImg.js
@@ -1,8 +1,27 @@
-    function showImg(){
-      return document.getElementById('img').src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png';
-    }
+   ////////////////////////////////////////// VERSION ALPHA //////////////////////////////////
+    //BETHA: PUT A GRID AND STYLES TO THE GALERY
+    //DELTA: SAVE IMG IN A LOCAL STORAGE OR DATA BASE.
+/*
+   <h1 id="title">Manejo Dom</h1>
+    <button onclick="showImg()">Show</button>
 
-   function AddImg(img){
+    <form id="formImg" action="">
+       Link of Img: <input type="text" name="link" value="url">
+    </form>
+    <button onclick="AddImg()">Add</button>
+
+
+    <br>
+    <div class="galery">
+        <img style="width: 350px;" id="img" src="https://as1.ftcdn.net/v2/jpg/01/21/03/32/1000_F_121033205_DjPchsBLyPTJCEIbNBpkafsQMlPh7U5A.jpg" alt="">
+
+*/
+
+function showImg(){
+      return document.getElementById('img').src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png';
+}
+
+function AddImg(img){
     let formImg = document.forms['formImg'];
     let link = formImg['link'].value;
     console.log(link);
@@ -15,4 +34,4 @@
     let galery = document.getElementById('galery')
     galery.appendChild(img)
     return document.getElementById('img').src = img;
-    }
+}
